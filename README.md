0c134c80fa300f82639bc055548baff774bce7b3
Xenador77 <river@xenador-hermes.com>
2022-11-30 00:48:21 -0500

    Initial installation of Docusaurus
    Signed-off-by: Xenador77 <river@xenador-hermes.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# teroshdl-docusaurus-doc
+#TerosHDL Docs built with Docusaurus  
+Very WIP and no claims as to the accuracy, relevance, or value gained by reading these docs has been made. You have been warned and will be the only person liable in the event of any misfortunes experienced, money lost, property damaged, or end-times hastened as a result of not heeding this warning and/or reading any part of this documentation. 
+
+The official current documentation is <here>(https://github.com/TerosTechnology/terosHDLdoc)
---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'teroshdl', // Usually your GitHub org/user name.
+  projectName: 'teroshdl-docusaurus-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+          },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'TerosHDL-Docusaurus-Docs',
+        logo: {
+          alt: 'TerosHDL logo',
+          src: 'img/logo.svg', /**replace with teroshdl logo */
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/teroshdl',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/xenador77',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/xenador77',
+              },
+            ],
+          },
+        ],
+        copyright: `GPLv3 ${new Date().getFullYear()} TerosHDL-Docusaurus-Docs, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
